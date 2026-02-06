ed343ac4435bb92a053fef3d986d5908bc50a01c
FlyinButter1 <gabriellisiecki1@gmail.com>
2026-02-06 20:43:17 +0100

    Add api url validation

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,3 +1,3 @@
 // Ensure no trailing slash for consistency
-export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000/api/v1/"
+export const API_URL = (import.meta.env.VITE_API_URL || "http://localhost/api/v1/").replace(/\/?$/, '/');
 
